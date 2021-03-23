33403fc1a6ff225ae4e2f788a58aea35d62e6985
SolomidHero <dim.sad23@gmail.com>
2021-03-23 19:49:02 +0300

    spec configuration tests added

---
+++ spec/localhost/pip_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+
+# ml packages
+describe package('torch') do
+  it { should be_installed.by('pip').with_version('1.7.1') }
+end
+
+describe package('torch-optimizer') do
+  it { should be_installed.by('pip').with_version('0.1.0') }
+end
+
+
+# tests
+describe package('pytest') do
+  it { should be_installed.by('pip').with_version('6.2.2') }
+end
+
+
+# configurations
+describe package('omegaconf') do
+  it { should be_installed.by('pip').with_version('2.0.6') }
+end
+
+describe package('hydra-core') do
+  it { should be_installed.by('pip').with_version('1.0.6') }
+end
+
+
+# audio processing and feature extraction
+describe package('librosa') do
+  it { should be_installed.by('pip').with_version('0.8.0') }
+end
+
+describe package('torchaudio') do
+  it { should be_installed.by('pip').with_version('0.7.2') }
+end
+
+describe package('transformers') do
+  it { should be_installed.by('pip').with_version('4.3.2') }
+end
+
+describe package('pyworld') do
+  it { should be_installed.by('pip').with_version('0.2.12') }
+end
+
+describe package('numpy') do
+  it { should be_installed.by('pip') }
+end
+
+describe package('scipy') do
+  it { should be_installed.by('pip') }
+end
---
+++ spec/localhost/util_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+
+describe file('/usr/local/lib/pkgconfig/sndfile.pc') do
+  it { should exist }
+end
+
+
+describe file('/proc/driver/nvidia/version') do
+  it { should exist }
+end
+
+
+describe package('nvcc') do
+  it { should be_installed }
+end
